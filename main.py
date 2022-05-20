affed877fd696bc4abed0579fc0e18330e4c663b
stoicastefan <67829760+stoicastefan@users.noreply.github.com>
2022-05-20 11:29:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ link_loss  = []
 possible_high_att = {}
 high_att = {}
 very_high_att = {}
+link_loss_sort = {}
 buildings_with_att = []
 
 source = requests.get('http://monitor.jcs.jo/onu/activated_onu.php').text
@@ -79,12 +80,22 @@ with requests.Session() as s:
             raise Exception
 
         # Deschidem deranjament pentru link loss
+        print('----------------------   LINK LOSS  --------------------')
         for mac_onu in link_loss:
-            print(f'Clientul cu locatia {mac_onu.get_location} si pon: {} ')
+            client = erp(mac_onu, s)
+            link_loss_sort[mac_onu] =  client.get_pon()
+            
+
+        OrderedDict(link_loss_sort)
+        for mac_onu in link_loss_sort:
+            client = erp(mac_onu, s)
+            print(f' Clientul cu locatia: {client.get_location()} si pon: {link_loss_sort[mac_onu]} este link_loss')
+        print('------------------------------------------------------- ')
         print(f'Vrei sa deschizi deranjament la {len(link_loss)} clienti cu link loss? (y/n)')
         confirmation = str(input())
         if(confirmation == 'y'):
             for mac_onu in link_loss:
+                
                 client = erp(mac_onu, s)
                 contract = client.get_contract()
                 location = client.get_location()
@@ -103,6 +114,11 @@ with requests.Session() as s:
         
         high_att = OrderedDict(high_att)
         
+        print('----------------------   HIGH ATT  --------------------')
+        for mac_onu in high_att:
+            client = erp(mac_onu, s)
+            print(f'Clientul cu locatia: {client.get_location()} si pon: {client.get_pon()} are atenuarea: {high_att[mac_onu]}')
+        print('---------------------- --------------------------------')
         print(f'Cate deranjamente pentru atenuari deschid(sunt {len(high_att)})')
         numar_atenuari = int(input())
 
@@ -135,20 +151,21 @@ with requests.Session() as s:
                     if(max(attenuations_of_neighbours.values()) - min(attenuations_of_neighbours.values()) <= 2.5):
                             buildings_with_att.append(location)
                             print(f'atenuare pe cladirea {location}')
+                            mail.open_deranjament_attbuilding(location)
                             continue
                     else:
                         if(numar_atenuari > 0):
                             mail.open_deranjament('High att', contract )
                             numar_atenuari = numar_atenuari-1
                             --numar_atenuari
-                            print(f'Contractul clientului cu Mac ONU:  {mac_onu} este {contract} in locatia {location} (high att)')
+                            print(f'Am deschid deranjament pentru  Mac ONU:  {mac_onu} cu contractul: {contract} in locatia {location} (high att)')
                         
                 elif(contract != '654000025'):
                     if(numar_atenuari > 0):
                             mail.open_deranjament('High att', contract )
                             numar_atenuari = numar_atenuari-1
                             --numar_atenuari
-                            print(f'Contractul clientului cu Mac ONU:  {mac_onu} este {contract} in locatia {location} (high att)')
+                            print(f'Am deschid deranjament pentru  Mac ONU:  {mac_onu} cu contractul: {contract} in locatia {location} (high att)')
 
         # Deschidem deranjament cu link loss pentru
         # clientii cu atenuare peste 33.5
@@ -168,12 +185,13 @@ with requests.Session() as s:
 
         if (buildings_with_att):
             mail.open_deranjament_attbuilding("\n".join(buildings_with_att))
-                
+              
 
     except Exception as e:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(e).__name__, e.args)
         print(message)
+
 
 
 
